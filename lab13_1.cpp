8d874e3b0150f271aff625e0192e166bb8455c38
boss <theeraphan_p@cmu.ac.th>
2023-01-10 15:20:10 +0700

    13.1 completed

--- lab13_1.cpp
+++ lab13_1.cpp
@@ -1,9 +1,18 @@
 #include<iostream>
 using namespace std;
 
+int fibonacci(int);
+
 int main(){
     int x;
     cin >> x;
     cout << fibonacci(x);
     return 0; 
+}
+
+int fibonacci(int i){
+
+if(i <= 1) return i;
+else return fibonacci(i-1) + fibonacci(i-2);
+
 }
